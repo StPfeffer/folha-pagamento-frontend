fa5f1b8f1785eb2bb59000aeb74ea04d840553c9
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 19:14:06 -0300

    Componente da Sidebar

---
+++ src/components/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,51 @@
+import React from "react"
+
+import styles from "./sidebar.module.css";
+import { menuItems } from "@/config/routes/menuItems";
+import MenuLink from "./menuLink/menuLink";
+import LogoutIcon from '@mui/icons-material/Logout';
+import Image from "next/image";
+
+const Sidebar = async () => {
+  // const { user } = await auth();
+  return (
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
+        <div className={styles.userDetail}>
+          <span className={styles.username}>John Doe</span>
+          <span className={styles.userTitle}>Administrator</span>
+        </div>
+      </div>
+      <ul className={styles.list}>
+        {menuItems.map((cat) => (
+          <li key={cat.title}>
+            <span className={styles.cat}>{cat.title}</span>
+            {cat.list.map((item) => (
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
+      <form
+        // action={async () => {
+        //   "use server";
+        //   await signOut();
+        // }}
+      >
+        <button className={styles.logout}>
+          <LogoutIcon />
+          Logout
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Sidebar;
