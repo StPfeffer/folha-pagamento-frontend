9397a58f12a9226e8efa93212591c4eb01eaff5b
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-03-19 19:12:19 -0300

    Layout da aplicação

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Navbar from '@/components/dashboard/navbar/navbar';
+import styles from '@/components/dashboard/dashboard.module.css';
+import Footer from '@/components/dashboard/footer/footer';
+import Sidebar from '@/components/dashboard/sidebar/sidebar';
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <Sidebar />
+      </div>
+
+      <div className={styles.content}>
+        <Navbar />
+          {children}
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Layout;
